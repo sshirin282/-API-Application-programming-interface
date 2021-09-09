d65dc25704b83c59ded7ffc7a02c37ec52c25ea7
azhar <shaikhnoorish8@gmail.com>
2021-09-09 15:44:19 +0530

    Application programming interface

--- app/src/main/java/com/example/api/MainActivity.kt
+++ app/src/main/java/com/example/api/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.api
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.widget.Toolbar
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -11,12 +12,16 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     lateinit var buttomnavigation:BottomNavigationView
-
+    lateinit var toolbar:Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         buttomnavigation=findViewById(R.id.bottomnavigation)
+        toolbar=findViewById(R.id.toolbar)
+        setTitle("Application Programming Interface")
+        setSupportActionBar(toolbar)
+
 
         buttomnavigation.setOnNavigationItemSelectedListener {
             when(it.itemId){
--- app/src/main/java/com/example/api/ap1.kt
+++ app/src/main/java/com/example/api/ap1.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
